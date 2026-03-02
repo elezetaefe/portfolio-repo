75354f10f578eeb915f6d32ba4db4bf0c5e4b944
elezetaefe <ele.zeta.efe@gmail.com>
2026-03-02 13:48:08 +0100

    Logo añadido que da a la página HomeLZF

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,49 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router';
-
+import LogoLZF from '@/components/logoLZF.vue';
 
 
 </script>
 
 <template>
 
-  <header class="bg-[#ffde00] w-full border border-black">
-        <!-- header/barra de navegación -->
-    <div 
-    class="flex flex-col md:flex-row items-center justify-between p-3 mx-auto max-w-350 md:px-20"> 
-    <!-- div para el estilo de como se escriben las cosas en el header -->
+  <div class="grid grid-rows-[auto_1fr_auto] min-h-screen">
 
-    <!-- <LogoSimpsons estilus="w-[8rem] h-auto"/> para insertar el logotipo -->
+    <header class="bg-[#a6c89a] w-full ">
+          <!-- header/barra de navegación -->
 
-        <!-- div para espaciar los botones bien -->
-        <div class="flex gap-6 pt-3">
-                <RouterLink to="/projects" active-class="font-bold">
-                    Projects
-                </RouterLink>
+      <!-- div para el estilo de como se escriben las cosas en el header -->
+      <div 
+      class="flex flex-col md:flex-row items-center justify-start p-3 mx-auto max-w-450 md:px-10"> 
 
-                <RouterLink to="/aboutMe" active-class="font-bold">
-                    About me
-                </RouterLink>
-        </div>
-
-    </div>
+      <!-- <LogoSimpsons estilus="w-[8rem] h-auto"/> para insertar el logotipo -->
         
-  </header>
+        <RouterLink to="/">
+          <LogoLZF estilus="w-[15rem] pr-10"/>
+        </RouterLink>
 
-  <main>   
-    <RouterView />
-  </main>
+
+          <!-- div para espaciar los botones bien -->
+          <div class="flex items-center gap-6 ">
+                  <RouterLink to="/projects" active-class="font-bold">
+                      Projects
+                  </RouterLink>
+
+                  <RouterLink to="/aboutMe" active-class="font-bold">
+                      About me
+                  </RouterLink>
+          </div>
+
+      </div>
+          
+    </header>
+
+    <main>   
+      <RouterView />
+    </main>
+
+
+  </div>
 </template>
 
 <style scoped>
--- src/pages/home/HomeLZF.vue
+++ src/pages/home/HomeLZF.vue
@@ -1,4 +1,6 @@
 <script lang="ts" setup>
+
+
 // import { scrollToSection } from '@/utils/ScrollToSection';
 
 
@@ -8,8 +10,7 @@
 <template>
     <div class="grid grid-rows-[auto_1fr_auto] min-h-screen">
 
-        
-
+       
             <h1 class="min-h-screen text-center p-4 flex flex-col items-center justify-center text-2xl">
                 hola soy Laura
             </h1>        
