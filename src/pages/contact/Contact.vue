baf05f1c97d89562f5847fd14d69faa79902379f
elezetaefe <ele.zeta.efe@gmail.com>
2026-03-08 11:44:07 +0100

    página contacto hecha

--- src/pages/contact/Contact.vue
+++ src/pages/contact/Contact.vue
@@ -1,79 +1,97 @@
 
 <script setup lang="ts">
-import Calendar from '@/components/ui/calendar/Calendar.vue';
 import Input from '@/components/ui/input/Input.vue';
-import Popover from '@/components/ui/popover/Popover.vue';
-import PopoverContent from '@/components/ui/popover/PopoverContent.vue';
-import PopoverTrigger from '@/components/ui/popover/PopoverTrigger.vue';
 import Select from '@/components/ui/select/Select.vue';
 import SelectContent from '@/components/ui/select/SelectContent.vue';
 import SelectItem from '@/components/ui/select/SelectItem.vue';
 import SelectTrigger from '@/components/ui/select/SelectTrigger.vue';
 import SelectValue from '@/components/ui/select/SelectValue.vue';
-
-import type { DateValue } from 'reka-ui';
-import { ref } from 'vue';
-
-
-    const dies = ref<DateValue>()
+import { InstagramIcon, Mail } from 'lucide-vue-next';
 
 </script>
 
 <template>
 
-    <div class="container mx-auto max-w-5xl px-4 bg-amber-400">
+    <div class="flex justify-center container mx-auto max-w-5xl px-4 my-20">
 
-    <div class="flex justify-start items-center w-auto bg-amber-300 h-auto p-8">
-        <h1 class="text-5xl font-bold mt-8">Contact me</h1>
-    </div>
+        <div class="flex flex-col justify-center items-start w-auto h-auto p-8">
+            <h1 class="text-5xl font-bold mt-8 mb-6">Contact me</h1>
+
+            <div class="flex flex-col justify-start w-auto">
+                <div class="flex flex-row items-center mb-3">
+                    <Mail class="w-5 h-5 mr-3 stroke-[#5e904d]"/>
+                    <p class="text-[#5e904d] font-bold">
+                        ele.zeta.efe@gmail.com
+                    </p>
+                </div>
+
+                <div class="flex flex-row items-center">
+                    <InstagramIcon class="w-5 h-5 mr-3 stroke-[#5e904d]"/>
+                    <a href="https://www.instagram.com/ele.zeta.efe.dis/" target="_blank" 
+                    class="hover:underline text-[#5e904d] font-bold">
+                        ele.zeta.efe.dis
+                    </a>
+                </div>
+            </div>
+        </div>
 
     <div class="w-full max-w-md mx-auto lg:max-w-none lg:w-1/2 ">
-        <form  class="space-y-6 bg-white p-8 rounded-lg shadow-lg aspect-square">
+        <form  class="space-y-6 bg-[#f6fff2] p-8 rounded-lg shadow-lg aspect-square m-10">
             <div class="space-y-2"> 
-                <Label for="nomen">Nombre</Label>
+                <Label for="nomen">Full name</Label>
                     <Input id="nomen" required />
             </div>
-                            
+
             <div class="space-y-2">
-                <Label for="cognomen">Apellidos</Label>
-                    <Input id="cognomen" required />
+                <Label for="email">E-mail</Label>
+                    <Input id="email" required />
             </div>
 
             <div class="space-y-2">
 
                 <Select required>
                     <SelectTrigger class="border-gray-200 bg-white text-gray-900">
-                        <SelectValue placeholder="Selecciona un tema a tratar" />
+                        <SelectValue placeholder="Select an issue" />
                     </SelectTrigger>
 
                     <SelectContent class="bg-white">
-                        <SelectItem value="rescatare">
-                            Comisión diseño
+                        <SelectItem value="design">
+                            Design commission
                         </SelectItem>
 
-                        <SelectItem value="investigare">
-                            Comisión ilustración 
+                        <SelectItem value="ilustr">
+                            Illustrator commission 
                         </SelectItem>
 
-                        <SelectItem value="persequi">
-                            Propuesta de proyecto
+                        <SelectItem value="project">
+                            Project offer
                         </SelectItem>
 
-                        <SelectItem value="defendere">
-                            Solicitud de información
+                        <SelectItem value="info">
+                            I need information
+                        </SelectItem>
+
+                        <SelectItem value="other">
+                            Other (please specify below)
                         </SelectItem>
                     </SelectContent>
                 </Select>
             </div>
 
+            <div class="space-y-2">
+                <Label for="text">Comentario</Label>
+                <Input id="text" />
+            </div>
            
-
-            <Button 
-            type="submit"
-            class="w-full bg-[rgb(106,90,205)] hover:[rgb(88,75,171)] text-white text-md mt-4"
-            >
-                Enviar solicitud
-            </Button>
+            <div class="flex justify-center w-full">
+                <Button 
+                type="submit"
+                class="w-80 bg-[#48784bdf] hover:bg-[#a6c89a] text-white text-md mt-4 rounded-xl"
+                >
+                    Enviar solicitud
+                </Button>
+            </div>
+            
                         
         </form>
     </div>
